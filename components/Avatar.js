98fd32acd169774e4b34b363f60dffad4e73c77b
Azeem Khan <azeemkhanreal@gmail.com>
2022-04-25 17:59:27 +0530

    initial commit

---
+++ components/Avatar.js
@@ -0,0 +1,11 @@
+const Avatar = ({ url }) => {
+  return (
+    <img
+      className="h-10 rounded-full transition duration-150 transform hover:scale-110"
+      loading="lazy"
+      src={url}
+      alt="profile pic"
+    />
+  );
+};
+export default Avatar;
